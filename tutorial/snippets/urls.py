6dd67bc8628729b4b427b331392c895c9924ce97
Vadzim Herasimovich <vadim.gerasimovich@gmail.com>
2020-05-01 20:42:12 +0300

    Create endpoints for the root of the api and highlighted snippets.

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -4,6 +4,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
+    path('', views.api_root),
+    path('snippets/<int:pk>/highlight', views.SnippetHighlight.as_view()),
     path('snippets/', views.SnippetList.as_view()),
     path('snippets/<int:pk>', views.SnippetDetail.as_view()),
     path('users/', views.UserList.as_view()),
